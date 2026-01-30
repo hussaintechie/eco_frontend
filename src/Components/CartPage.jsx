1e8d4b030c700aa5c8451600dd0a94a70f8e9202
hussaintechie <rajhussain1042003@gmail.com>
2026-01-30 21:47:33 +0530

    oaky

--- src/Components/CartPage.jsx
+++ src/Components/CartPage.jsx
@@ -357,17 +357,18 @@ const isSlotInFuture = (slotLabel) => {
   // -------------------------------
   // UPDATE QTY
   // -------------------------------
-  const updateQty = async (cart_id, qty) => {
-    if (qty < 1) return;
-    try {
-      await updateCartQtyAPI(cart_id, qty);
-      await loadCart();
-      await loadBill();
-    } catch (err) {
-      console.error("Error updating quantity:", err);
-    }
-  };
-  const handleClearCart = async () => {
+const updateQty = async (cart_id, qty) => {
+  try {
+    // ✅ allow qty = 0
+    await updateCartQtyAPI(cart_id, qty);
+
+    await loadCart();
+    await loadBill();
+  } catch (err) {
+    console.error("Error updating quantity:", err);
+  }
+};
+ handleClearCart = async () => {
     try {
       const res = await clearCartAPI();
       console.log(res.data.message);
